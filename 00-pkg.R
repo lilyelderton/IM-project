d801f3518d918f634ad8dedc73362260800a69a7
Lily Elderton <lilyelderton35@gmail.com>
2022-11-10 10:08:59 +0000

    load packages

---
+++ 00-pkg.R
@@ -0,0 +1,8 @@
+## Packages that need to be loaded ## 
+
+library(pdftools)     # to read in PDF files 
+library(tm)           # various text mining functions
+library(readr)        # read in the words from txt documents 
+library(tidyverse)    # for general data manipulation 
+library(stringr)      # for %>% 
+library(tidytext)     # convert between matrices and dataframes
