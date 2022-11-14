2db57bc98d5c16b8f8dbe568be713d081b8a8a7a
Lily Elderton <lilyelderton35@gmail.com>
2022-11-14 17:50:03 +0000

    write figure to file

--- 02-dates-published.R
+++ 02-dates-published.R
@@ -1,6 +1,6 @@
 ## comparing use of ip and pni in the field over time ## 
 
-# how many articles were published in each year 
+# how many articles were published in each year? Add count to dataframe 
 ip_date_count <- ip_data %>% 
   group_by(Year) %>% 
   mutate(count = n())
@@ -26,26 +26,39 @@ ggplot(data = dates, aes(x = Year, y = n, fill = Topic)) +
   theme(axis.text.x = element_text(angle = 90, vjust = 0.5)) 
 
 # overlay barplot (doesn't sum the count)
-ggplot() +
-  geom_bar(data = pni_date_count, aes(x = Year, y = count,
+fig1 <- ggplot() +
+  geom_bar(data = pni_date_count, aes(x = Year, y = count,     # barplot for pni dates 
                                      fill = Topic), 
            stat = "identity", position = "identity") +
-  geom_bar(data = ip_date_count, aes(x = Year, y = count,
+  geom_bar(data = ip_date_count, aes(x = Year, y = count,      # barplot for ip dates on top 
                                      fill = Topic), 
            stat = "identity", position = "identity", color = "black") +
   scale_y_continuous(expand = c(0,0), 
                      limits = c(0, 160),
-                     breaks = seq(0, 160, 10)) +
+                     breaks = seq(0, 160, 10)) +  # change the amount of axis ticks 
   scale_x_continuous(limits = c(1944, 2025), 
                      breaks = seq(1940, 2025, 5)) +
   scale_fill_manual(values = c("steelblue2", "gray87")) +
   ggtitle("The Use of Immunopsychiatry and Psychoneuroimmunology Over Time") +
   ylab("Number of Articles Published") +
-  theme_classic(base_family = "Times", 
-                base_size = 14) + 
+  theme_classic(base_size = 15) + 
   theme(axis.text.x = element_text(angle = 75, vjust = 0.5)) 
 
+# figure saving settings
+units <- "in"  # inches
+fig_w <- 10
+fig_h <- fig_w
+dpi <- 500
+device <- "png"   
 
+# save figure 
+ggsave("figures/fig1.png",
+       plot = fig1,
+       device = device,
+       width = fig_w,
+       height = fig_h,
+       units = units,
+       dpi = dpi)
 
 
 
