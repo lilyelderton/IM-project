a3ba2e1d0dab8746aeba41e44db6038371635050
Lily Elderton <lilyelderton35@gmail.com>
2022-11-14 17:50:48 +0000

    moved file

--- stopwords.R
+++
@@ -1,8 +0,0 @@
-## stop words ## 
-
-# read in extra sets of stopwords
-clinpsy_stopwords <- read_lines("stopwords/clin_psy_stopwords.txt") # common clinical psychiatry words 
-
-numbers_stopwords <- read_lines("stopwords/numbers_stopwords.txt")  # numbers in data that aren't important 
-
-add_stopwords <- c(clinpsy_stopwords, numbers_stopwords)
