fb08e0e21510ddef1986b21291013ca8e301bb6e
Lily Elderton <lilyelderton35@gmail.com>
2022-11-14 11:53:52 +0000

    made a stacked and overlaid barplot of IP and PNI dates

--- 02-dates-published.R
+++ 02-dates-published.R
@@ -24,6 +24,7 @@ records_pni_3000 <- EUtilsGet(search_pni_3000)
 ip_data <- data.frame("ID" = ArticleId(records_ip), 
                       "Year" = YearPubmed(records_ip), 
                       "Title" = ArticleTitle(records_ip))
+ip_data$Topic <- "Immunopsychiatry" # need to mark that these are immunopsychiatry
 
 pni_data_1000 <- data.frame("ID" = ArticleId(records_pni_1000), 
                        "Year" = YearPubmed(records_pni_1000), 
@@ -38,6 +39,7 @@ pni_data_3000 <- data.frame("ID" = ArticleId(records_pni_3000),
                             "Title" = ArticleTitle(records_pni_3000))
 
 pni_data <- rbind(pni_data_1000, pni_data_2000, pni_data_3000) # combine pni dfs
+pni_data$Topic <- "Psychoneuroimmunology"
 
 # how many articles were published in each year 
 ip_date_count <- ip_data %>% 
@@ -51,13 +53,37 @@ pni_date_count <- pni_data %>%
 pni_date_count$Topic <- "Psychoneuroimmunology"
 
 # bind the two dataframes
-dates <- rbind(ip_date_count, pni_date_count)
+dates <- rbind(ip_data, pni_data) %>% 
+  count(Year, Topic)
 
-# create a barplot 
-ggplot(data = dates, aes(x = Year, y = count, fill = Topic)) +
-  geom_bar(stat="identity", position ="dodge") 
+# stacked barplot
+ggplot(data = dates, aes(x = Year, y = n, fill = Topic)) +
+  geom_bar(stat= "identity", position = position_stack(reverse = TRUE)) +
+  scale_y_continuous(expand = c(0,0), 
+                     limits = c(0, 200)) +
+  scale_x_continuous(limits = c(1940, 2025)) +
+  scale_fill_manual(values = c("darksalmon", "darkolivegreen")) +
+  ggtitle("The Use of Immunopsychiatry and Psychoneuroimmunology Over Time") +
+  ylab("Number of Articles Published") +
+  theme_classic(base_family = "Times") + 
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5)) 
 
-
+# overlay barplot (doesn't sum the count)
+ggplot() +
+  geom_bar(data = pni_date_count, aes(x = Year, y = count,
+                                     fill = Topic), 
+           stat = "identity", position = "identity") +
+  geom_bar(data = ip_date_count, aes(x = Year, y = count,
+                                     fill = Topic), 
+           stat = "identity", position = "identity") +
+  scale_y_continuous(expand = c(0,0), 
+                     limits = c(0, 160)) +
+  scale_x_continuous(limits = c(1940, 2025)) +
+  scale_fill_manual(values = c("blue", "gray")) +
+  ggtitle("The Use of Immunopsychiatry and Psychoneuroimmunology Over Time") +
+  ylab("Number of Articles Published") +
+  theme_classic(base_family = "Times") + 
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5)) 
 
 
 
