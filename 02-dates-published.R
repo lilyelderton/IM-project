ab76d4fca858c7142c366ef2863d97ed4fd4587f
Lily Elderton <lilyelderton35@gmail.com>
2022-11-14 12:41:45 +0000

    changes to perfect overlay plot

--- 02-dates-published.R
+++ 02-dates-published.R
@@ -1,56 +1,13 @@
 ## comparing use of ip and pni in the field over time ## 
 
-# set topics for search on pubmed
-ip_pm <- "immunopsychiatry OR immuno-psychiatry" # try to catch all that come up in pubmed
-pni_pm <- "psychoneuroimmunology OR psycho-neuro-immunology" 
-
-# search for topics on pubmed 
-search_ip <- EUtilsSummary(ip_pm)
-summary(search_ip) # collects 148 
-
-search_pni_1000 <- EUtilsSummary(pni_pm, retstart = 0, retmax = 1000)     # pni needs to be split because its too large for RISmed
-search_pni_2000 <- EUtilsSummary(pni_pm, retstart = 1000, retmax = 1000)
-search_pni_3000 <- EUtilsSummary(pni_pm, retstart = 2000, retmax = 1005)
-
-# use EUtislGet to fetch the actual data 
-records_ip <- EUtilsGet(search_ip)
-
-records_pni_1000 <- EUtilsGet(search_pni_1000)
-records_pni_2000 <- EUtilsGet(search_pni_2000)
-records_pni_3000 <- EUtilsGet(search_pni_3000)
-
-# collect ID, year and title in a dataframe
-# pni will then need to be combined into one dataframe
-ip_data <- data.frame("ID" = ArticleId(records_ip), 
-                      "Year" = YearPubmed(records_ip), 
-                      "Title" = ArticleTitle(records_ip))
-ip_data$Topic <- "Immunopsychiatry" # need to mark that these are immunopsychiatry
-
-pni_data_1000 <- data.frame("ID" = ArticleId(records_pni_1000), 
-                       "Year" = YearPubmed(records_pni_1000), 
-                       "Title" = ArticleTitle(records_pni_1000))
-
-pni_data_2000 <- data.frame("ID" = ArticleId(records_pni_2000), 
-                            "Year" = YearPubmed(records_pni_2000), 
-                            "Title" = ArticleTitle(records_pni_2000))
-
-pni_data_3000 <- data.frame("ID" = ArticleId(records_pni_3000), 
-                            "Year" = YearPubmed(records_pni_3000), 
-                            "Title" = ArticleTitle(records_pni_3000))
-
-pni_data <- rbind(pni_data_1000, pni_data_2000, pni_data_3000) # combine pni dfs
-pni_data$Topic <- "Psychoneuroimmunology"
-
 # how many articles were published in each year 
 ip_date_count <- ip_data %>% 
   group_by(Year) %>% 
   mutate(count = n())
-ip_date_count$Topic <- "Immunopsychiatry" # need to mark that these are immunopsychaitry
 
 pni_date_count <- pni_data %>% 
   group_by(Year) %>% 
   mutate(count = n())
-pni_date_count$Topic <- "Psychoneuroimmunology"
 
 # bind the two dataframes
 dates <- rbind(ip_data, pni_data) %>% 
@@ -75,15 +32,18 @@ ggplot() +
            stat = "identity", position = "identity") +
   geom_bar(data = ip_date_count, aes(x = Year, y = count,
                                      fill = Topic), 
-           stat = "identity", position = "identity") +
+           stat = "identity", position = "identity", color = "black") +
   scale_y_continuous(expand = c(0,0), 
-                     limits = c(0, 160)) +
-  scale_x_continuous(limits = c(1940, 2025)) +
-  scale_fill_manual(values = c("blue", "gray")) +
+                     limits = c(0, 160),
+                     breaks = seq(0, 160, 10)) +
+  scale_x_continuous(limits = c(1944, 2025), 
+                     breaks = seq(1940, 2025, 5)) +
+  scale_fill_manual(values = c("steelblue2", "gray87")) +
   ggtitle("The Use of Immunopsychiatry and Psychoneuroimmunology Over Time") +
   ylab("Number of Articles Published") +
-  theme_classic(base_family = "Times") + 
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5)) 
+  theme_classic(base_family = "Times", 
+                base_size = 14) + 
+  theme(axis.text.x = element_text(angle = 75, vjust = 0.5)) 
 
 
 
