b1c2409a1e8d150ce532c27edaf235207d763b61
Lily Elderton <lilyelderton35@gmail.com>
2022-11-16 16:41:40 +0000

    wrote to file

--- 03-tokenise-abstracts.R
+++ 03-tokenise-abstracts.R
@@ -20,26 +20,6 @@ pni_abstracts_tidy <- pni_data %>%
   anti_join(add_stopwords_df) %>% 
   filter(is.na(as.numeric(word)))  # remove lone numbers 
 
-# find the most common words 
-ip_abstracts_tidy %>% 
-  count(word, sort = TRUE) # word counts stored in a tidy data frame 
-
-pni_abstracts_tidy %>% 
-  count(word, sort = TRUE)
-
-# visualise the most common words
-ip_abstracts_tidy %>% 
-  count(word, sort = TRUE) %>% 
-  filter(n > 50) %>% 
-  mutate(word = reorder(word, n)) %>% 
-  ggplot(aes(n, word)) +
-  geom_col() +
-  labs(y = NULL)
-
-pni_abstracts_tidy %>% 
-  count(word, sort = TRUE) %>% 
-  filter(n > 700) %>% 
-  mutate(word = reorder(word, n)) %>% 
-  ggplot(aes(n, word)) +
-  geom_col() +
-  labs(y = NULL)
+# write to file 
+write.csv(ip_abstracts_tidy, "data-tidy/ip_tokenised_abstracts.csv")
+write.csv(pni_abstracts_tidy, "data-tidy/pni_tokenised_abstracts.csv")
