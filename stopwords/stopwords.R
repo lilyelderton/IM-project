2aece08ba900e7620a8367f1aa36eb9a8c230c7c
Lily Elderton <lilyelderton35@gmail.com>
2022-11-17 10:22:46 +0000

    change to full_stopwords

--- stopwords/stopwords.R
+++ stopwords/stopwords.R
@@ -16,16 +16,8 @@ colnames(add_stopwords_df) <- "word"  # need to match the col name with the othe
 # stopwords for full text 
 # can see they are needed as they skew the top words in the articles 
 
-full_stopwords <- c("research", "university", "including", 
-                    "references", "data", "found", "evidence", 
-                    "received", "potential", "model", "department", 
-                    "authors", "role", "accepted", "article", "institute", 
-                    "published", "time", "online", "national", "future", 
-                    "recent", "levels", "due", "models", "risk", "methods", 
-                    "shown", "observed", "test", "mechanisms", "relationship", 
-                    "multiple", "biol", "tion", "supported", "support", "similar", 
-                    "level", "information", "factor", "factors", "differences",
-                    "author", "activity")
+full_stopwords <- c("data", "research", "model", "fig", "university", "models", "association",
+                    "participants")
 
 full_stopwords <- as.data.frame(full_stopwords)
 colnames(full_stopwords) <- "term"
