ca8d99ea0239924537112952c75aa263e0a33ad8
Lily Elderton <lilyelderton35@gmail.com>
2022-11-17 10:22:23 +0000

    wordcloud, remove tf-id as not good to visualise

--- 05-ip-full.R
+++ 05-ip-full.R
@@ -6,7 +6,7 @@ full_count <- ip_full_tidy %>%
 
 # plot the top 30 words 
 full_count %>% 
-  top_n(30) %>% 
+  top_n(40) %>% 
   ggplot(aes(x = fct_reorder(term, n), y = n)) +
   geom_col(colour = "black",  fill = "steelblue2") +
   geom_text(vjust = 0.5, hjust = 1.5,
@@ -17,13 +17,13 @@ full_count %>%
   scale_fill_manual(values = c("steelblue2","gray87")) +
   labs(y = "Count", 
        x = "Words") +
-  theme_bw(base_size = 13) # a lot of these words are filler
+  theme_bw(base_size = 13) # some of these words are filler
                            # would tell us more if we removed them 
 
 # remove stopwords then try again 
 full_count %>% 
   anti_join(full_stopwords) %>% 
-  top_n(30) %>% 
+  top_n(40) %>% 
   ggplot(aes(x = fct_reorder(term, n), y = n)) +
   geom_col(colour = "black",  fill = "steelblue2") +
   geom_text(vjust = 0.5, hjust = 1.5,
@@ -34,34 +34,16 @@ full_count %>%
   scale_fill_manual(values = c("steelblue2","gray87")) +
   labs(y = "Count", 
        x = "Words") +
-  theme_bw(base_size = 13)  # this however may not be the best method 
-                            # might be better to examine tf-id
+  theme_bw(base_size = 13)  
 
-# add term frequency and total words to ip_full_tidy
-pdf_words <- ip_full_tidy %>% 
-  count(document, term, wt = count, sort = TRUE)
+# make a wordcloud 
+wordcloud_fig <- wordcloud2(data = full_count, size = 1.6, 
+           color = rep_len(c("darkslateblue", "slateblue", "royalblue",
+                             "cornflowerblue", "steelblue"), 
+                           nrow(full_count)))
 
-total_words_pdf <- pdf_words %>% 
-  group_by(document) %>% 
-  summarise(total = sum(n))
-
-pdf_words <- left_join(pdf_words, total_words_pdf)  
-  
-# examine zipf's law 
-freq_by_rank <- pdf_words %>% 
-  group_by(document) %>% 
-  mutate(rank = row_number(), 
-         `term frequency` = n/total) %>% 
-  ungroup()
-
-# calculate tf-idf and add to df
-pdf_tf_idf <- pdf_words %>% 
-  bind_tf_idf(term, document, n)
-
-# look at words with high tf_idf = words that are important to a document 
-pdf_tf_idf %>% 
-  select(-total) %>% 
-  arrange(desc(tf_idf))
+# save graph (first to html, then in png) 
+saveWidget(wordcloud_fig,"wordcloud.html",selfcontained = F)
 
 
 
